b5f0bfd54e2c5ac2c267559817415808fd6ff906
Tri Hoang Minh <tri.hoang.hcmut@gmail.com>
2022-01-03 12:12:59 +0700

    styling table

---
+++ src/components/table/Table.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './table.css';
+
+function Table(props) {
+	return (
+		<div>
+			<div className="table-wrapper">
+				<table>
+					{props.headData && props.renderHead ? (
+						<thead>
+							<tr> {props.headData.map((item, index) => props.renderHead(item, index))}</tr>
+						</thead>
+					) : null}
+					{props.bodyData && props.renderBody ? (
+						<tbody>{props.bodyData.map((item, index) => props.renderBody(item, index))}</tbody>
+					) : null}
+				</table>
+			</div>
+		</div>
+	);
+}
+
+export default Table;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,154 @@
 import React from 'react';
 
+import Chart from 'react-apexcharts';
+import { Link } from 'react-router-dom';
+
 import statusCards from '../assets/JsonData/status-card-data.json';
 
 import StatusCard from '../components/status-card/StatusCard';
+
+import Table from '../components/table/Table';
+
+const chartOptions = {
+	series: [
+		{
+			name: 'Online Customers',
+			data: [40, 70, 20, 90, 36, 80, 30, 91, 60, 120],
+		},
+		{
+			name: 'Store Customers',
+			data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+		},
+	],
+	options: {
+		color: ['#6ab04c', '#2980b9'],
+		chart: {
+			background: 'transparent',
+		},
+		dataLabels: {
+			enabled: false,
+		},
+		stroke: {
+			curve: 'smooth',
+		},
+		xaxis: {
+			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+		},
+		legend: {
+			position: 'bottom',
+		},
+		grid: {
+			show: false,
+		},
+	},
+};
+
+const topCustomers = {
+	head: ['user', 'total orders', 'total spending'],
+	body: [
+		{
+			username: 'john doe',
+			order: '490',
+			price: '$15,870',
+		},
+		{
+			username: 'frank iva',
+			order: '250',
+			price: '$12,251',
+		},
+		{
+			username: 'anthony baker',
+			order: '120',
+			price: '$10,840',
+		},
+		{
+			username: 'frank iva',
+			order: '110',
+			price: '$9,251',
+		},
+		{
+			username: 'anthony baker',
+			order: '80',
+			price: '$8,840',
+		},
+	],
+};
+
+const renderCustomerHeader = (item, index) => <th key={index}>{item}</th>;
+
+const renderCustomerBody = (item, index) => (
+	<tr key={index}>
+		<td>{item.username}</td>
+		<td>{item.order}</td>
+		<td>{item.price}</td>
+	</tr>
+);
+
+const latestOrders = {
+	header: ['order id', 'user', 'total price', 'date', 'status'],
+	body: [
+		{
+			id: '#OD1711',
+			user: 'john doe',
+			date: '17 Jun 2021',
+			price: '$900',
+			status: 'shipping',
+		},
+		{
+			id: '#OD1712',
+			user: 'frank iva',
+			date: '1 Jun 2021',
+			price: '$400',
+			status: 'paid',
+		},
+		{
+			id: '#OD1713',
+			user: 'anthony baker',
+			date: '27 Jun 2021',
+			price: '$200',
+			status: 'pending',
+		},
+		{
+			id: '#OD1712',
+			user: 'frank iva',
+			date: '1 Jun 2021',
+			price: '$400',
+			status: 'paid',
+		},
+		{
+			id: '#OD1713',
+			user: 'anthony baker',
+			date: '27 Jun 2021',
+			price: '$200',
+			status: 'refund',
+		},
+	],
+};
+
+const orderStatus = {
+	shipping: 'primary',
+	pending: 'warning',
+	paid: 'success',
+	refund: 'danger',
+};
+
+const renderOrderHead = (item, index) => {
+	return <th key={index}>{item}</th>;
+};
+
+const renderOrderBody = (item, index) => {
+	return (
+		<tr>
+			<td>{item.id}</td>
+			<td>{item.user}</td>
+			<td>{item.price}</td>
+			<td>{item.date}</td>
+			<td>
+				<span>{item.status}</span>
+			</td>
+		</tr>
+	);
+};
 
 const Dashboard = () => {
 	return (
@@ -21,7 +167,47 @@ const Dashboard = () => {
 					</div>
 				</div>
 				<div className="col-6">
-					<div className="card full-height">{/* chart */}</div>
+					<div className="card full-height">
+						<Chart options={chartOptions.options} series={chartOptions.series} type="line" height="100%" />
+						{/* chart */}
+					</div>
+				</div>
+				<div className="col-5">
+					<div className="card">
+						<div className="card__header">
+							<h3>top customers</h3>
+						</div>
+						<div className="card__body">
+							<Table
+								headData={topCustomers.head}
+								bodyData={topCustomers.body}
+								renderBody={renderCustomerBody}
+								renderHead={renderCustomerHeader}
+							/>
+							{/* table */}
+						</div>
+						<div className="card__footer">
+							<Link to="/customers">view all</Link>
+						</div>
+					</div>
+				</div>
+				<div className="col-7">
+					<div className="card">
+						<div className="card__header">
+							<h3>latest orders</h3>
+						</div>
+						<div className="card__body">
+							<Table
+								headData={latestOrders.header}
+								bodyData={latestOrders.body}
+								renderBody={renderOrderBody}
+								renderHead={renderCustomerHeader}
+							/>
+						</div>
+						<div className="card__footer">
+							<Link to="/">view all</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>

174979d386351e8697a8b007ddcb7b8e93c08309
Gia Duong <duonggia31@gmail.com>
2023-05-19 21:23:11 +0700

    first commit

--- src/api/signalR.js
+++ src/api/signalR.js
@@ -2,7 +2,7 @@ import { HubConnectionBuilder, JsonHubProtocol } from '@microsoft/signalr';
 
 const connection = new HubConnectionBuilder()
 	//add domain hub here
-	.withUrl('https://example.com/hub')
+	.withUrl('https://localhost:44309/hub')
 	.withHubProtocol(new JsonHubProtocol())
 	.withAutomaticReconnect([0, 10, 10000])
 	.build();
